a237db2eb60c50bb4ffca6a8d7b3e06b45954379
SHISH <dmit.nujdin2014@yandex.ru>
2022-07-02 13:45:07 +0400

    Added scheduling

--- src/main/java/org/nuzhd/Main.java
+++ src/main/java/org/nuzhd/Main.java
@@ -5,19 +5,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.devtools.restart.Restarter;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.File;
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableScheduling
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Europe/Kirov")
+    public void restartApp() {
+        Restarter.getInstance().restart();
     }
 
     @EventListener
